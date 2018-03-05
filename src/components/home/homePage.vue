421c9e2b781bb4be263650821792438db0149933
yanglx <1071126434@qq.com>
2018-03-05 17:33:41 +0800

    粒子

--- src/components/home/homePage.vue
+++ src/components/home/homePage.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="homePage">
-    homePage
+    <ul class="scrollBox">
+      <li>
+        <particles></particles>
+      </li>
+    </ul>
   </div>
 </template>
 <script type="text/ecmascript-6">
+import Particles from '../canvas/particles'
 export default {
   name: 'homePage',
+  components: {
+    Particles
+  },
   data () {
     return {
     }
@@ -13,4 +21,20 @@ export default {
 }
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
+.homePage
+  position fixed
+  top 0
+  left 0
+  width 100%
+  height 100%
+  overflow hidden
+  ul
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    li
+      width 100%
+      height 100%
 </style>
